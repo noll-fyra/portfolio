e79003bcb282ab73b701be81f7c7d02dad08f24b
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-06-06 19:44:20 +0800

    fix project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -21,23 +21,23 @@ class Project extends React.Component {
         <ul>
           <li className='projectOption' onClick={() => this.props.handleProject(0)}>
             <div className='projectDiv project0' />
-            <div className='modal'>Population: One</div>
+            <div className={'modal ' + (this.state.project === 0 ? 'modalActive' : 'modalInactive')}>Population: One</div>
           </li>
           <li className='projectOption' onClick={() => this.props.handleProject(1)}>
             <div className='projectDiv project1' />
-            <div className='modal'>Locavorus</div>
+            <div className={'modal ' + (this.state.project === 1 ? 'modalActive' : 'modalInactive')}>Locavorus</div>
           </li>
           <li className='projectOption' onClick={() => this.props.handleProject(2)}>
             <div className='projectDiv project2' />
-            <div className='modal'>Love Confidently</div>
+            <div className={'modal ' + (this.state.project === 2 ? 'modalActive' : 'modalInactive')}>Love Confidently</div>
           </li>
           <li className='projectOption' onClick={() => this.props.handleProject(3)}>
             <div className='projectDiv project3' />
-            <div className='modal'>Locavorus Rex</div>
+            <div className={'modal ' + (this.state.project === 3 ? 'modalActive' : 'modalInactive')}>Locavorus Rex</div>
           </li>
           <li className='projectOption' onClick={() => this.props.handleProject(4)}>
             <div className='projectDiv project4' />
-            <div className='modal'>Via Postale</div>
+            <div className={'modal ' + (this.state.project === 4 ? 'modalActive' : 'modalInactive')}>Via Postale</div>
           </li>
         </ul>
         <div className='projectMain'>

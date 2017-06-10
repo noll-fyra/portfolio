ae38433093462c98647a0403040515028238c9aa
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-06-10 12:54:22 +0800

    about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,21 +3,36 @@ import './about.css'
 
 const About = (props) => (
   <div className='about'>
-    <div className='aboutStarter'>
-      <div className='profileContainer'>
-        <div className='profileImage' />
-        <div className='profileDetails'>
-          <h3 className='tagline'>Hi! I'm Jonathan.</h3>
-          <div className='tagline'>
-            <span className='tagline'>Problem Solver. Happiness Creator. Efficiency Guru.</span>
-          </div>
+    <div className='aboutStarter aboutDiv'>
+      <div className='profileImage' />
+      <div className='profileDetails'>
+        <div className='tagline taglineLarge'>
+          Hi! I'm Jonathan Louis Ng.
+        </div>
+        <div className='tagline taglineSmall'>
+          Problem Solver. Happiness Creator. Efficiency Guru.
         </div>
       </div>
-      <div className='aboutWords'>
-        I work on projects that address real world issues, making peoples' lives better and happier, and try to do all this as efficiently and effectively as possible. I enjoy learning new ways to code, and I derive massive satisfaction from solving programming challenges.
-      </div>
     </div>
-    <div className='aboutEnder'>
+    <div className='aboutWords aboutDiv'>
+      <p>I work on projects that address real world issues, making peoples' lives better and happier, and try to do all this as efficiently and effectively as possible. I enjoy learning new ways to code, and I derive massive satisfaction from solving programming challenges.</p>
+      <button className='hitMeUp' onClick={() => props.handleShowing('contact')}>Get in touch if you want to make a difference</button>
+    </div>
+    <div className='aboutSkills aboutDiv'>
+      <i className='devicon-javascript-plain skillIcon' />
+      <i className='devicon-react-original-wordmark skillIcon' />
+      <i className='devicon-html5-plain-wordmark skillIcon' />
+      <i className='devicon-css3-plain-wordmark skillIcon' />
+      <i className='devicon-rails-plain-wordmark skillIcon' />
+      <i class='devicon-jquery-plain-wordmark skillIcon' />
+      <i className='devicon-nodejs-plain-wordmark skillIcon' />
+      <i className='devicon-express-original-wordmark skillIcon' />
+      <i className='devicon-mongodb-plain-wordmark skillIcon' />
+      <i className='devicon-postgresql-plain-wordmark skillIcon' />
+      <i className='devicon-github-plain-wordmark skillIcon' />
+      <i className='devicon-heroku-original-wordmark skillIcon' />
+    </div>
+    <div className='aboutEnder aboutDiv'>
       <span onClick={() => props.handleShowing('none')} className='fa fa-times-circle' />
     </div>
   </div>

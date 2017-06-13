29b3086a47df0d43ea542289127e7813b4a7c403
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-06-13 11:32:51 +0800

    project details

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-##### Jonathan Louis Ng
+# Jonathan Louis Ng
 _[Portfolio](http://www.jonathanlouisng.com)_
 
 My portfolio was created with React; the animated balloons were copied wholesale (with minimal tweaks) from a [pen](https://codepen.io/bennettfeely/details/nbFCp) by [Bennett Feely](https://codepen.io/bennettfeely/), while the icons were by [Devicon](http://konpa.github.io/devicon/) and [Font Awesome](http://fontawesome.io).
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,11 +3,11 @@ import './projects.css'
 
 const text =
   [
-    'Population: One is a top-down zombie survival game for two players. The objective is to be the last player standing after a relentless zombie horde attacks. Players have to dodge increasing numbers of zombies as well as fixed obstacles, aided only by 1 of 6 special abilities.',
-    'Locavorus aims to manage the complete life cycle of hospitality customer service, from before they patronise the business to well after their meal. In its current form, it smooths the customer ordering process by sending their orders directly to the kitchen in real time.',
-    'Finished in one night for the UN Women Hackathon, Love Confidently aims to help women in developing countries by educating them about contraception, making it easy to find and use contraceptives, tracking their condition and connecting them with medical professionals.',
-    'Locavorus Rex is an application for restaurants and other food businesses to manage their reservations, queue and orders. It aims to improve customer service by making various aspects of the service life cycle more efficient.',
-    'Via Postale is a travel-focused social media site that is best described as Instagram x Pinterest x TripAdvisor. Users post their travel experiences, complete with description and rating. Other users can in turn search for activities and places, saving those that interest them to plan future trips.'
+    ['Population: One is a top-down zombie survival game for two players. The objective is to be the last player standing after a relentless zombie horde attacks. Players have to dodge increasing numbers of zombies as well as fixed obstacles, aided only by 1 of 6 special abilities.', 'A chilling soundtrack and ominous countdown forewarn the danger that lies ahead. A helpful hint tries to add levity to a serious situation.', 'Avoid the obstacles and the zombies and outrun your opponent to win. Use your limited special power charges sparingly.'],
+    ['Locavorus aims to manage the complete life cycle of hospitality customer service, from before they patronise the business to well after their meal.', 'Customers can make service requests directly to the staff without having to signal for their attention, reducing inefficiency and increasing customer satisfaction.', 'Orders are sent directly to the kitchen, so customers get their food more quickly and more accurately. Dietary notes can be passed as well.', 'The restaurant can manage their menu here, to keep track of their offerings, while letting customers view the food options online.', 'Locavorus caters to both customers and restaurants.'],
+    ['Finished in one night for the UN Women Hackathon, Love Confidently aims to help women in developing countries by educating them about contraception, making it easy to find and use contraceptives, tracking their condition and connecting them with medical professionals.', 'All women have different needs and respond differently to contraception. We set up a simple survey to help narrow down the most appropriate and relevant method for each woman, while also allowing them to browse and compare the different methods on their own.', 'We used a simple geolocation implementation to find all medical supply stores near you, which would ideally provide information about what stores carried which contraceptives and how much they cost.', 'We implemented socket.io to enable real time communication. The idea was that women in need could contact a medical professional to get immediate advice on their contraception or just to address any concerns they might have.', 'We used Twilio SMS to implement a tracking feature where women opting in would receive a message every day at the same time.', 'The response would be tracked and a follow-up message might be sent depending on the patient\'s history.'],
+    ['Locavorus Rex is an application for restaurants and other food businesses to manage their reservations, queue and orders. It aims to improve customer service by making various aspects of the service life cycle more efficient.', 'Customers can make reservations quickly and easily, and an email confirmation is sent soon after.', 'Queuing is hassle-free. Just enter your details and be contacted when your table is ready. You can check your estimated wait at any time.', 'The system automatically notifies you when your wait is over.', 'Customers can also make takeaway orders online.', 'Easy online payment is integrated via Stripe.', 'Businesses can manage their reservations, queues and orders from a dashboard.', 'The kitchen receives orders directly, which are prioritised automatically by time, and include future orders e.g. takeaways and reservations.', 'Restaurants can also get a quick glance at their active and completed orders/invoices.'],
+    ['Via Postale is a travel-focused social media site that is best described as Instagram x Pinterest x TripAdvisor. Users post their travel experiences, complete with description and rating. Other users can in turn search for activities and places, saving those that interest them to plan future trips.', 'Users post images of their activities. We try to prefill the fields from image EXIF data to simplify the process.', 'Users can view other users\' trips and activities and follow those they like.', 'Users can save activities they like for future reference.', 'Users can plan their own trips around activities they have saved.']
   ]
 
 const builtWith =
@@ -37,11 +37,21 @@ const sites =
     'https://via-priori.firebaseapp.com'
   ]
 
+const images =
+  [
+      ['http://i.imgur.com/79eNOe5.png', 'http://i.imgur.com/rKxKPHf.png', 'http://i.imgur.com/xtVG7dy.png'],
+      ['http://i.imgur.com/zBILyBo.png', 'http://i.imgur.com/hSIMlk9.png', 'http://i.imgur.com/BhJ3mN3.png', 'http://i.imgur.com/RUlN3fr.png', 'http://i.imgur.com/KEQb77r.png'],
+      ['http://i.imgur.com/YI1ajgW.png', 'http://i.imgur.com/Elij0x5.png', 'http://i.imgur.com/CHJ5yRy.png', 'http://i.imgur.com/opHQRxm.png', 'http://i.imgur.com/Jt9IBn7.png', 'http://i.imgur.com/B2nD434.png'],
+      ['http://i.imgur.com/NrsI8Ue.png', 'http://i.imgur.com/ItgnGDt.png', 'http://i.imgur.com/VNEfRAt.png', 'http://i.imgur.com/ljKzWwo.png', 'http://i.imgur.com/PZEJ5xC.png', 'http://i.imgur.com/3bGzLoI.png', 'http://i.imgur.com/iCsmqur.png', 'http://i.imgur.com/NimOPLj.png', 'http://i.imgur.com/B1IQLuQ.png'],
+      ['http://i.imgur.com/6OwwM9u.png', 'http://i.imgur.com/Acs6znn.png', 'http://i.imgur.com/Hmgnwz4.png', 'http://i.imgur.com/oR5PR31.png', 'http://i.imgur.com/Hw4HkEc.png']
+  ]
+
 class Project extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       project: props.project,
+      page: [0, 0, 0, 0, 0],
       hovering: false
     }
     this.handlePagination = this.handlePagination.bind(this)
@@ -55,12 +65,16 @@ class Project extends React.Component {
   }
 
   handlePagination (direction) {
+    let currentPage = this.state.page[this.props.project]
+    let currentState = this.state.page
     if (direction === 'forward') {
-      this.state.project === 4 ? this.props.handleProject(0) : this.props.handleProject(this.state.project + 1)
+      currentState[this.props.project] = currentPage === (images[this.props.project].length - 1) ? 0 : currentPage + 1
     } else {
-      this.state.project === 0 ? this.props.handleProject(4) : this.props.handleProject(this.state.project - 1)
+      currentState[this.props.project] = currentPage === 0 ? (images[this.props.project].length - 1) : currentPage - 1
     }
-    console.log(direction)
+    this.setState({
+      page: currentState
+    })
   }
 
   handleHovering () {
@@ -98,10 +112,10 @@ class Project extends React.Component {
           <div className='projectDetails' onClick={() => this.handlePagination('back')}>
             <span className='fa fa-angle-left' />
           </div>
-          <div className={'projectImage project' + this.state.project} />
+          <div className='projectImage' style={{backgroundImage: `url(${images[this.state.project][this.state.page[this.state.project]]})`}} />
           <div className='projectCommentsContainer' onMouseOver={this.handleHovering} onMouseOut={this.handleHovering}>
             <div className='projectComments'>
-              {text[this.state.project]}
+              {text[this.state.project][this.state.page[this.state.project]]}
             </div>
             <div className='projectBuiltWith'>
               {builtWith[this.state.project]}

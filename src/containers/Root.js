dd64adaf306961ac1a9cab11c2320d84dfe5eed3
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2018-07-04 10:14:44 +0800

    hide poll until after match starts

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
-import { Switch, BrowserRouter as Router, Route, Redirect, NavLink } from 'react-router-dom'
+import {
+  Switch,
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  NavLink
+} from 'react-router-dom'
 import firebase from 'firebase/app'
 import 'firebase/database'
 import 'firebase/auth'
@@ -32,7 +38,7 @@ const hidden = ['newpoll']
 
 class Root extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       data: {},
       number: null,
@@ -42,117 +48,271 @@ class Root extends Component {
     this.unregisterAuthObserver = null
     this.uiConfig = {
       callbacks: {
-          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            window.localStorage.setItem('number', authResult.user.phoneNumber)
-            this.setState({number: authResult.user.phoneNumber})
-            // console.log(authResult, redirectUrl, this.state);
-            return false;
-          }.bind(this),
-          // uiShown: function() {
-          //   // The widget is rendered.
-          //   // Hide the loader.
-          //   document.getElementById('loader').style.display = 'none';
-          // }
-        },
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-        signInFlow: 'popup',
-        // signInSuccessUrl: '<url-to-redirect-to-on-success>',
-        signInOptions: [
-          // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          // firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          {
-            provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-            defaultCountry: 'SG'
-          }
-        ],
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+          window.localStorage.setItem('number', authResult.user.phoneNumber)
+          this.setState({ number: authResult.user.phoneNumber })
+          // console.log(authResult, redirectUrl, this.state);
+          return false
+        }.bind(this)
+        // uiShown: function() {
+        //   // The widget is rendered.
+        //   // Hide the loader.
+        //   document.getElementById('loader').style.display = 'none';
+        // }
+      },
+      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+      signInFlow: 'popup',
+      // signInSuccessUrl: '<url-to-redirect-to-on-success>',
+      signInOptions: [
+        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        {
+          provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          defaultCountry: 'SG'
+        }
+      ]
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     database.ref('/').on('value', snap => {
-      this.setState({data: snap.val()})
-    // let polls = Object.assign({}, snap.val().polls)
-    // for (var key in polls) {
-    // let poll = polls[key]
-    // poll.users['+61430985344'] = {answer: 0, answered: true}
-    // polls[key] = poll
-  // }
-    // database.ref().child('polls').set(polls)
+      this.setState({ data: snap.val() })
+      // let users = Object.assign({}, snap.val().users)
+      // for (var key in polls) {
+      // let poll = polls[key]
+      // poll.users['+61430985344'] = {answer: 0, answered: true}
+      // polls[key] = poll
+      // }
+      // database.ref().child('polls').set(polls)
     })
 
-let number = window.localStorage.getItem('number')
-this.setState({hidden: !!number})
-    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-        (user) => {
-          // console.log(user)
-          if(!!user){
-          this.setState({number: user.phoneNumber})
-          window.localStorage.setItem('number', user.phoneNumber)
-        }
-        }
-    )
+    let number = window.localStorage.getItem('number')
+    this.setState({ hidden: !!number })
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      // console.log(user)
+      if (!!user) {
+        this.setState({ number: user.phoneNumber })
+        window.localStorage.setItem('number', user.phoneNumber)
 
+        database.ref('/users').once('value', snap => {
+          let updatedUser = Object.assign({}, snap.val()[user.phoneNumber])
+          updatedUser.updated = 'v1.1'
+          database
+            .ref('/')
+            .child('users/' + user.phoneNumber)
+            .update(updatedUser)
+        })
+      }
+    })
   }
 
   componentWillUnmount() {
     this.unregisterAuthObserver()
   }
 
-  removeNumber(){
-    this.setState({number: null, hidden: false})
+  removeNumber() {
+    this.setState({ number: null, hidden: false })
   }
 
   render() {
     return (
       <div>
-      {this.state.number ?
-      !!this.state.data.users && Object.keys(this.state.data.users).includes(this.state.number)
- ? <Router>
-  <div>
-    <h1 style={{textAlign: 'center', marginTop: '12px'}}>2018 FIFA WORLD CUP PREDICTION GAME</h1>
-  <Route path='/' render={() => <div style={{backgroundColor: '#19364C'}}>
-          <div style={{display: 'flex', justifyContent: 'center', width: '100%', maxWidth: '480px', margin: '12px auto'}}>
-          {sections.map(sect =>
-            <NavLink key={sect} to={`/${sect}`} activeStyle={{backgroundColor: 'gold', color: '#19364C'}} style={{width: `calc(100%/${sections.length})`, maxWidth: `calc(480px/${sections.length})`, fontWeight: 'bold', textAlign: 'center', color: 'white', textDecoration: 'none', padding: '8px'}}>
-              {sect[0].toUpperCase().concat(sect.slice(1))}
-            </NavLink>)}
+        {this.state.number ? (
+          !!this.state.data.users &&
+          Object.keys(this.state.data.users).includes(this.state.number) ? (
+            <Router>
+              <div>
+                <h1 style={{ textAlign: 'center', marginTop: '12px' }}>
+                  2018 FIFA WORLD CUP PREDICTION GAME
+                </h1>
+                <Route
+                  path="/"
+                  render={() => (
+                    <div style={{ backgroundColor: '#19364C' }}>
+                      <div
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'center',
+                          width: '100%',
+                          maxWidth: '480px',
+                          margin: '12px auto'
+                        }}>
+                        {sections.map(sect => (
+                          <NavLink
+                            key={sect}
+                            to={`/${sect}`}
+                            activeStyle={{
+                              backgroundColor: 'gold',
+                              color: '#19364C'
+                            }}
+                            style={{
+                              width: `calc(100%/${sections.length})`,
+                              maxWidth: `calc(480px/${sections.length})`,
+                              fontWeight: 'bold',
+                              textAlign: 'center',
+                              color: 'white',
+                              textDecoration: 'none',
+                              padding: '8px'
+                            }}>
+                            {sect[0].toUpperCase().concat(sect.slice(1))}
+                          </NavLink>
+                        ))}
+                      </div>
+
+                      {this.state.number === '+6587427184' && (
+                        <div
+                          style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            width: '100%',
+                            maxWidth: '480px',
+                            margin: '12px auto'
+                          }}>
+                          {hidden.map(sect => (
+                            <NavLink
+                              key={sect}
+                              to={`/${sect}`}
+                              activeStyle={{
+                                backgroundColor: 'gold',
+                                color: '#19364C'
+                              }}
+                              style={{
+                                width: `calc(100%/${hidden.length})`,
+                                maxWidth: `calc(480px/${hidden.length})`,
+                                fontWeight: 'bold',
+                                textAlign: 'center',
+                                color: 'white',
+                                textDecoration: 'none',
+                                padding: '8px'
+                              }}>
+                              {sect[0].toUpperCase().concat(sect.slice(1))}
+                            </NavLink>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  )}
+                />
+
+                <div style={{ padding: 12 }}>
+                  <Switch>
+                    <Route
+                      exact
+                      path="/"
+                      render={() => <Redirect to="/polls" />}
+                    />
+                    <Route
+                      path="/polls"
+                      render={() => (
+                        <Polls
+                          polls={this.state.data.polls}
+                          users={this.state.data.users}
+                          teams={this.state.data.teams}
+                          database={database}
+                          number={this.state.number}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/newpoll"
+                      render={() => (
+                        <NewPoll
+                          polls={this.state.data.polls}
+                          users={this.state.data.users}
+                          teams={this.state.data.teams}
+                          database={database}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/editpoll"
+                      render={() => (
+                        <EditPoll
+                          polls={this.state.data.polls}
+                          users={this.state.data.users}
+                          teams={this.state.data.teams}
+                          database={database}
+                          number={this.state.number}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/table"
+                      render={() => (
+                        <Table
+                          polls={this.state.data.polls}
+                          users={this.state.data.users}
+                          teams={this.state.data.teams}
+                          database={database}
+                          storage={firebase.storage()}
+                          number={this.state.number}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/matches"
+                      render={() => (
+                        <Matches
+                          matches={this.state.data.matches}
+                          teams={this.state.data.teams}
+                          database={database}
+                          number={this.state.number}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/teams"
+                      render={() => (
+                        <Teams
+                          matches={this.state.data.matches}
+                          teams={this.state.data.teams}
+                          database={database}
+                          number={this.state.number}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/logout"
+                      render={() => (
+                        <LogOut
+                          auth={firebase.auth()}
+                          removeNumber={this.logOut}
+                        />
+                      )}
+                    />
+                  </Switch>
+                </div>
+              </div>
+            </Router>
+          ) : (
+            <div
+              style={{
+                width: '100vw',
+                height: '100vh',
+                display: 'flex',
+                flexFlow: 'column',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}>
+              <i
+                className="fa fa-futbol-o fa-spin"
+                style={{ fontSize: '2em' }}
+              />
+              <h1>Loading</h1>
             </div>
-
-            {this.state.number === '+6587427184' &&
-            <div style={{display: 'flex', justifyContent: 'center', width: '100%', maxWidth: '480px', margin: '12px auto'}}>
-            {hidden.map(sect =>
-              <NavLink key={sect} to={`/${sect}`} activeStyle={{backgroundColor: 'gold', color: '#19364C'}} style={{width: `calc(100%/${hidden.length})`, maxWidth: `calc(480px/${hidden.length})`, fontWeight: 'bold', textAlign: 'center', color: 'white', textDecoration: 'none', padding: '8px'}}>
-                {sect[0].toUpperCase().concat(sect.slice(1))}
-              </NavLink>)}
-              </div>}
-            </div>}/>
-
-<div style={{padding: 12}}>
-          <Switch>
-            <Route exact path='/' render={() => <Redirect to='/polls' />} />
-            <Route path='/polls' render={() => <Polls polls={this.state.data.polls} users={this.state.data.users} teams={this.state.data.teams} database={database} number={this.state.number} />} />
-            <Route path='/newpoll' render={() => <NewPoll polls={this.state.data.polls} users={this.state.data.users} teams={this.state.data.teams} database={database} />} />
-            <Route path='/editpoll' render={() => <EditPoll polls={this.state.data.polls} users={this.state.data.users} teams={this.state.data.teams} database={database} number={this.state.number} />} />
-            <Route path='/table' render={() => <Table polls={this.state.data.polls} users={this.state.data.users} teams={this.state.data.teams} database={database} storage={firebase.storage()} number={this.state.number} />} />
-            <Route path='/matches' render={() => <Matches matches={this.state.data.matches} teams={this.state.data.teams} database={database} number={this.state.number} />} />
-            <Route path='/teams' render={() => <Teams matches={this.state.data.matches} teams={this.state.data.teams} database={database} number={this.state.number} />} />
-            <Route path='/logout' render={() => <LogOut auth={firebase.auth()} removeNumber={this.logOut} />} />
-          </Switch>
+          )
+        ) : (
+          <div style={{ width: this.state.hidden ? '0' : '100%' }}>
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
           </div>
-          </div>
-        </Router>
-        : <div style={{width: '100vw', height: '100vh', display: 'flex', flexFlow: 'column', justifyContent: 'center', alignItems: 'center'}}>
-          <i className='fa fa-futbol-o fa-spin' style={{fontSize: '2em'}}/>
-          <h1>Loading</h1>
-        </div>
-        : <div style={{width: this.state.hidden ? '0' : '100%'}}>
-          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
-          </div>
-      }
-        </div>
+        )}
+      </div>
     )
   }
 }

c414ee070c06f01894c923bd100bbaaa9f475545
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2019-02-15 08:49:29 +0800

    finish ourstory

--- src/containers/ourstory/Index.js
+++ src/containers/ourstory/Index.js
@@ -3,142 +3,173 @@ import styled from 'styled-components'
 
 const memories = [
   {
-    date: '27 August 2018',
-    story: '',
-    image:
-      'https://res.cloudinary.com/noll-fyra/image/upload/v1550032637/IMG_1177_nuek8y.jpg'
-  },
-  {
-    date: '28 September 2018',
+    date: '22 April 2017',
     story:
-      'The look of ecstasy I have on my face whenever you’re close. Biting = love',
+      'Just one month in to our course and I already had a major crush on you. I loved the way you smiled and the way you laughed and the way every room you entered became brighter.',
     image:
-      'https://res.cloudinary.com/noll-fyra/image/upload/v1550032635/e541e217-eda8-47d7-899a-030a68a5df3b_zfynxl.jpg'
+      'https://res.cloudinary.com/noll-fyra/image/upload/v1550190320/IMG_4524_u8xvef.png'
   },
   {
-    date: '',
-    story: '',
+    date: '28 August 2018',
+    story: '😍',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032634/IMG_2614_qvdzxd.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '6 July 2018',
+    story:
+      "I'm so happy that my family loves you almost as much as me because you are already family to me.",
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032634/IMG_2038_kuhweq.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '28 April 2018',
+    story:
+      'I joined taichi to spend more time with you and I haven’t regretted a single second.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032634/IMG_1714_ggzdac.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '25 November 2017',
+    story:
+      "It's so easy to be with you. I especially love that you always bring so much joy to everyone around you.",
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032609/IMG_0057_n5fcax.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '27 January 2018',
+    story:
+      'Thank you for being so supportive of everything I do. You have such a generous and giving heart.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032607/DSCF4998_t4mv7u.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '14 February 2018',
+    story:
+      'Your smile is more heart melting than a bouquet of beautiful flowers. I would do anything to make you smile.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032607/IMG_1027_orzilg.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '5 January 2018',
+    story: 'Looking to adopt.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032604/IMG_0647_ksgxxf.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '16 December 2017',
+    story:
+      'We became parents to a fat baby who takes up too much space on the bed.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032604/IMG_0407_mthnt0.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '17 March 2018',
+    story:
+      'You are an incredible inspiration to me. You not only overcame cancer, but that you do your best to help fellow survivors and sufferers always makes me so proud to be with you.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032602/IMG_1331_s5iak7.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '24 September 2017',
+    story:
+      'I wanted to be with you all the time. You brought the sun to Melbourne with your happiness during the day and I loved making you happy during the night.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032600/IMG_0826_u9ulnr.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '16 June 2017',
+    story:
+      'Sitting beside you helping you do your portfolio were some of my happiest moments ever. I didn’t know then that the woman with extremely short hair would turn out to be the love of my life.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032570/IMG_9386_qzn5zm.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '29 July 2017',
+    story:
+      'All I knew was that I was completely smitten. You are so beautiful.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032570/IMG_0032_wtkgjp.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '2 July 2017',
+    story:
+      'I saw your wild side for the first time and I was shocked. I wasn’t sure what I was getting myself into.',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032570/IMG_9687_spnqey.jpg'
   },
   {
-    date: '',
-    story: '',
+    date: '4 June 2017',
+    story:
+      'A bittersweet moment. We graduated from our course but that meant I was running out of time to tell you how I felt. ',
     image:
       'https://res.cloudinary.com/noll-fyra/image/upload/v1550032570/IMG_9192_xxmhko.jpg'
   },
+  // {
+  //   date: '',
+  //   story: '',
+  //   image:
+  //     'https://res.cloudinary.com/noll-fyra/image/upload/v1550032570/IMG_0609_a507fq.jpg'
+  // },
   {
-    date: '',
-    story: '',
+    date: '28 September 2018',
+    story:
+      'I love you so much I will pretend to have a look of ecstasy whenever you’re biting me.',
     image:
-      'https://res.cloudinary.com/noll-fyra/image/upload/v1550032570/IMG_0609_a507fq.jpg'
+      'https://res.cloudinary.com/noll-fyra/image/upload/v1550032635/e541e217-eda8-47d7-899a-030a68a5df3b_zfynxl.jpg'
+  },
+  {
+    date: '14 February 2019',
+    story:
+      'I love you with all my heart and I want to spend the rest of my life with you.',
+    image:
+      'https://res.cloudinary.com/noll-fyra/image/upload/v1550190316/IMG_7199_ovhhae.heic'
   }
 ]
 
 const photoSplit = 'https://res.cloudinary.com/noll-fyra/image/upload/'
-const photoTransform = 'c_scale,q_auto,w_600/'
+const photoTransform = 'c_scale,q_auto,w_600,f_jpg/'
 
 const Memory = ({ memory }) => (
   <MemoryContainer>
-    <Circle>
+    {/*  <Circle>
       <InnerCircle />
-    </Circle>
+    </Circle>*/}
     <ImageContainer>
       <Image
         src={photoSplit
           .concat(photoTransform)
           .concat(memory.image.split(photoSplit)[1])}
       />
-      <span>{memory.date}</span>
-      <span>{memory.story}</span>
+      <div style={{ backgroundColor: 'white', padding: '12px' }}>
+        <strong>{memory.date}</strong>
+        <br />
+        <br />
+        <span>{memory.story}</span>
+      </div>
     </ImageContainer>
   </MemoryContainer>
 )
 
 class OurStory extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
   render() {
     return (
       <Container>
-        <h1>Our Story</h1>
+        <h1 style={{ textAlign: 'center' }}>Our Story So Far</h1>
+        <br />
+        <br />
 
         <TimelineContainer>
-          <Timeline />
+          {/* <Timeline /> */}
           <div>
-            {memories.map(m => (
-              <Memory key={m.image} memory={m} />
-            ))}
+            {memories
+              .sort((a, b) => new Date(a.date) - new Date(b.date))
+              .map(m => (
+                <Memory key={m.image} memory={m} />
+              ))}
           </div>
         </TimelineContainer>
       </Container>
@@ -150,7 +181,7 @@ export default OurStory
 
 const Container = styled.div`
   padding: 12px;
-  margin-left: 24px;
+  /* margin-left: 24px; */
 `
 
 const Timeline = styled.div`
@@ -177,13 +208,14 @@ const TimelineContainer = styled.div`
 `
 
 const MemoryContainer = styled.div`
-  width: 100vw;
+  width: 100%;
   max-width: 640px;
   display: flex;
   align-items: flex-start;
+  margin: 0 auto;
   margin-bottom: 24px;
-  position: relative;
-  left: -24px;
+  /* position: relative; */
+  /* left: -24px; */
   padding: 2px;
 `
 
@@ -230,9 +262,9 @@ const InnerCircle = styled.div`
 const ImageContainer = styled.div`
   width: 100%;
   position: relative;
-  background-color: #00aabb;
+  border: 1px solid lightgrey;
+  /* background-color: #00aabb; */
   border-radius: 0.4em;
-  height: 188px;
   overflow: hidden;
 `
 
